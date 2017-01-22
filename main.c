98f2fdb4d1d4c7f6e7074e92bf4a741333ef38a6
teodora96 <galateica@hotmail.com>
2017-01-22 12:31:51 +0100

    Potvrda-funkcija

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// Prva aplikacija, za ulaz na autoput
 
-int main()
+void potvrda(int brUredjaja)
 {
+    ++brPotvrde;
+    time_t trenutno_vrijeme;
+    char* vrijeme;
+    trenutno_vrijeme = time(NULL);
+    vrijeme = ctime(&trenutno_vrijeme);
 
+    FILE *fp;
+    if((fp=fopen("potvrda.txt", "w")) !=NULL)
+    {
+        fprintf(fp, "\n                         -ROUTE 66-            ");
+        fprintf(fp, "\n\n   POTVRDA O ULASKU NA AUTOPUT\n");
+        fprintf(fp,"\n Datum   : ");
+        fprintf(fp,"%s ", vrijeme);
+        fprintf(fp,"\n Uredjaj : ");
+        fprintf(fp,"%d ", brUredjaja);
+        fprintf(fp,"\n ------------------------------------------------------------\n");
+        fprintf(fp,"\n                    POTVRDA br. %d ", brPotvrde);
+        fprintf(fp,"\n ------------------------------------------------------------\n");
+        fprintf(fp,"\n              HVALA, SRECAN PUT!        \n\n");
+        fclose(fp);
+    }
 }
